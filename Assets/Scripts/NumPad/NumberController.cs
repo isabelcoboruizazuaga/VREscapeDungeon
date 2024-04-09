1e9d8dd791bc13f48f6820053603e4f804b347ea
Isa <isabelcoboruizazuaga@gmail.com>
2024-04-09 18:04:03 +0200

    added functional number pad and fixed chess

--- Assets/Scripts/NumPad/NumberController.cs
+++ Assets/Scripts/NumPad/NumberController.cs
@@ -7,21 +7,15 @@ public class NumberController : MonoBehaviour
 {
     public int number = -1;
     public NumberPadController padController;
+
     // Start is called before the first frame update
     void Start()
     {
         padController=FindObjectOfType<NumberPadController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void RegisterTouch()
-    {
-        //Debug.Log(number);
-            padController.AddNumber(number, this.GetComponent<Toggle>());
+    {           
+        padController.AddNumber(number, this.GetComponent<Toggle>());
     }
 }
--- Assets/Scripts/NumPad/NumberPadController.cs
+++ Assets/Scripts/NumPad/NumberPadController.cs
@@ -8,71 +8,45 @@ public class NumberPadController : MonoBehaviour
 {
     public List<int> numberList = new List<int> ();
     public List<Toggle> toggleList = new List<Toggle> ();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void isNumberInList(int number)
-    {
-        ;
-    }
+    private string winningSequence = "1, 3, 6, 8";
+
     public void AddNumber(int number, Toggle toggle)
     {
         toggle.interactable = false;
-        if (numberList.Contains(number))
-        {
-            /*numberList.Remove(number);
-            toggle.isOn = false;
-            toggleList.Remove(toggle);*/
-        }
-        else
-        {
-            numberList.Add(number);
-            toggleList.Add(toggle);
-        }
-        if(numberList.Count == 4)
+        numberList.Add(number);
+        toggleList.Add(toggle);
+
+        if (numberList.Count == 4)
         {
             string sequence= numberList[0]+ ", "+ numberList[1]+ ", "+ numberList[2]+ ", "+ numberList[3];
 
             Debug.Log(sequence);
 
-            if (sequence != "1, 2, 3, 4"){
-                StartCoroutine(Wait());
+            if (sequence != winningSequence){
+                StartCoroutine(IncorrectCode());
             }
             else
             {
-                StartCoroutine(Wait());
-            }
-            
-            
+                Debug.Log("OpenDoor!");
+                Debug.Log("*Door sound*");
+            }     
         }
     }
-    IEnumerator Wait()
+    IEnumerator IncorrectCode()
     {
+        numberList.Clear();
+
         yield return new WaitForSeconds(1f);
         Debug.Log("lista toggle: "+toggleList.Count);
 
         for (var i = 0; i < toggleList.Count; i++)
         {
             toggleList[i].isOn = false;
+            //Aqui se cambiaría el icono si no se esconde
             toggleList[i].interactable = true;
         }
 
-        /*toggleList[0].isOn = false;
-        toggleList[1].isOn = false;
-        toggleList[2].isOn = false;
-        toggleList[3].isOn = false;
-        toggleList.Clear();
-        numberList.Clear();
-
-        toggle.interactable = true;*/
         Debug.Log("lista toggle: " + toggleList.Count);
         Debug.Log("________________________________");
 
