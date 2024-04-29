3cd6a6c9739ff17514f9a2eac3523d59caba6b84
Isa <isabelcoboruizazuaga@gmail.com>
2024-04-29 17:54:52 +0200

    added change to raycast distance in lava room

--- Assets/Scripts/RoomEntryDetector.cs
+++ Assets/Scripts/RoomEntryDetector.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class RoomEntryDetector : MonoBehaviour
 {
+    public bool changeRay = false;
+    public GameObject rayObject;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             GetComponentInParent<DoorsController>().StartRoom();
+            if(changeRay)
+            {
+                changeXray();
+            }
             Destroy(this.gameObject,0.5f);
         }
+    }
+
+    private void changeXray()
+    {
+        rayObject.GetComponent<XRRayInteractor>().maxRaycastDistance = 1.5f;
+        rayObject.GetComponent<XRInteractorLineVisual>().lineLength = 1.5f;
+
     }
 }
