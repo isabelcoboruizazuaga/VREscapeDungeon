8559dc112a009be24d8c95afdb31c8db4185b9e6
Isa <isabelcoboruizazuaga@gmail.com>
2024-05-09 09:36:07 +0200

    testing colliders

--- Assets/Scripts/LavaPuzzle/LavaScript.cs
+++ Assets/Scripts/LavaPuzzle/LavaScript.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class LavaScript : MonoBehaviour
 {
     public Transform respawn;
+    public GameObject platformController;
+
+    private Transform platformTransform;
+
+    private void Start()
+    {
+        platformTransform = platformController.transform;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
             other.transform.position= respawn.position;
             other.transform.rotation = respawn.rotation;
+
+            platformController.transform.position = platformTransform.position;
+            platformController.transform.rotation = platformTransform.rotation;
         }
     }
 }
